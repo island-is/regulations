17ac7f7a73c010a8c4cce8eb83afb4e7e2b01b11
Valur Sverrisson <valur@hugsmidjan.is>
2021-03-24 12:06:23 +0000

    fix: ministries order

--- src/db/Ministry.ts
+++ src/db/Ministry.ts
@@ -4,6 +4,11 @@ import { getConnection } from 'typeorm';
 export async function getAllMinistries() {
   const connection = getConnection();
   const ministryRepository = connection.getRepository(Ministry);
-  const ministries: Array<Ministry> = (await ministryRepository.find()) ?? [];
+  const ministries: Array<Ministry> = await ministryRepository
+    .createQueryBuilder('regulations')
+    .orderBy('current', 'DESC')
+    .addOrderBy('`order`', 'ASC')
+    .addOrderBy('name', 'ASC')
+    .getMany();
   return ministries;
 }
