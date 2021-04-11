76ae9cf693fe84db19de35485d0a93501a0b66dd
mar <mar.nospam@anomy.net>
2021-04-11 16:18:17 +0000

    chore: Run `typeorm-model-generator` via npx

--- generate-model.sh
+++ generate-model.sh
@@ -3,5 +3,5 @@ cd "./${0%/*}" # cd into project root
 
 export $(egrep -v '^#' .env | xargs)
 
-typeorm-model-generator -h ${MYSQL_HOST} -d ${MYSQL_DB} -u ${MYSQL_USER} -x ${MYSQL_PASS} -p ${MYSQL_PORT} -e mysql
+npx typeorm-model-generator -h ${MYSQL_HOST} -d ${MYSQL_DB} -u ${MYSQL_USER} -x ${MYSQL_PASS} -p ${MYSQL_PORT} -e mysql
 rm ./src/ormconfig.json ./src/tsconfig.json
