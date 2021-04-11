2543b34c1cbd9c1273efcb3a1cef21c24e4c645b
mar <mar.nospam@anomy.net>
2021-04-11 16:07:02 +0000

    chore: Read .env when type-generation script starts

--- generate-model.sh
+++ generate-model.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+cd "./${0%/*}" # cd into project root
+
+export $(egrep -v '^#' .env | xargs)
+
 typeorm-model-generator -h ${MYSQL_HOST} -d ${MYSQL_DB} -u ${MYSQL_USER} -x ${MYSQL_PASS} -p ${MYSQL_PORT} -e mysql
 rm ./src/ormconfig.json ./src/tsconfig.json
