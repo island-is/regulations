2aa6d7f442393c0fc2da01432c4ac6ba04c05e4e
mar <mar.nospam@anomy.net>
2021-04-19 11:36:17 +0000

    fix: Disable setting headers while investigating error

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -108,8 +108,8 @@ export const assertPosInt = (maybeNumber: string): IntPositive | undefined => {
 
 const HOURS = 60 * 60;
 export const cache = (res: FastifyReply, ttl_hrs: number): void => {
-  res.headers({
-    'Content-Type': 'text/css; charset=UTF-8',
-    'Cache-Control': 'public, max-age=' + ttl_hrs * HOURS + (ttl_hrs ? ', immutale' : ''),
-  });
+  // res.headers({
+  //   'Content-Type': 'text/css; charset=UTF-8',
+  //   'Cache-Control': 'public, max-age=' + ttl_hrs * HOURS + (ttl_hrs ? ', immutale' : ''),
+  // });
 };
