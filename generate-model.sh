15ce7f9561316e2d85e397881017d2790055d8a0
Valur Sverrisson <valur@hugsmidjan.is>
2021-04-20 14:07:54 +0000

    fix: remove unnecessary deps

--- generate-model.sh
+++ generate-model.sh
@@ -3,4 +3,4 @@ cd "./${0%/*}" # cd into project root
 
 export $(egrep -v '^#' .env | xargs)
 
-stg -h ${MYSQL_HOST} -d ${MYSQL_DB} -u ${MYSQL_USER} -x ${MYSQL_PASS} -p ${MYSQL_PORT} -D mysql -T DBMeta -c -o ./src/models
+npx stg -h ${MYSQL_HOST} -d ${MYSQL_DB} -u ${MYSQL_USER} -x ${MYSQL_PASS} -p ${MYSQL_PORT} -D mysql -T DBMeta -c -o ./src/models
