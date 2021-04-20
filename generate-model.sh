ca8b38654ab24cc331ab4231e60deddba4aec475
mar <mar.nospam@anomy.net>
2021-04-20 18:06:10 +0000

    chore: Run sequelize-typescript-generator using npx

--- generate-model.sh
+++ generate-model.sh
@@ -3,4 +3,4 @@ cd "./${0%/*}" # cd into project root
 
 export $(egrep -v '^#' .env | xargs)
 
-stg -h ${MYSQL_HOST} -d ${MYSQL_DB} -u ${MYSQL_USER} -x ${MYSQL_PASS} -p ${MYSQL_PORT} -D mysql -T DBMeta,AuditLog,regulationarticle -c -o ./src/models
+npx sequelize-typescript-generator -h ${MYSQL_HOST} -d ${MYSQL_DB} -u ${MYSQL_USER} -x ${MYSQL_PASS} -p ${MYSQL_PORT} -D mysql -T DBMeta,AuditLog,regulationarticle -c -o ./src/models
