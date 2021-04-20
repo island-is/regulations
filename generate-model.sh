9570f7ef251cfb483dd7d8593c9c2c1314081d82
mar <mar.nospam@anomy.net>
2021-04-20 18:41:09 +0000

    docs: Add fixme and use long-form parameters

--- generate-model.sh
+++ generate-model.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-cd "./${0%/*}" # cd into project root
+# FIXME: check if script is run without any folder path
+# (i.e. `sh generate-model.sh` instead of `sh ./generate-model.sh`)
+# and skip this cd step
+cd "./${0%/*}/." # cd into project root
 
 export $(egrep -v '^#' .env | xargs)
 
-npx sequelize-typescript-generator -h ${MYSQL_HOST} -d ${MYSQL_DB} -u ${MYSQL_USER} -x ${MYSQL_PASS} -p ${MYSQL_PORT} -D mysql -T DBMeta,AuditLog,regulationarticle -c -o ./src/models
+npx sequelize-typescript-generator@^4 --host ${MYSQL_HOST} --database ${MYSQL_DB} --username ${MYSQL_USER} --password ${MYSQL_PASS} --port ${MYSQL_PORT} --dialect mysql --skip-tables DBMeta,AuditLog,regulationarticle --clean --out-dir ./src/models
