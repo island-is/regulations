69a480402d06bba888e5b4a7470e35b5569a7520
mar <mar.nospam@anomy.net>
2021-04-21 11:25:54 +0000

    docs: Echo warning after generate-model runs

--- generate-model.sh
+++ generate-model.sh
@@ -7,3 +7,5 @@ cd "./${0%/*}/." # cd into project root
 export $(egrep -v '^#' .env | xargs)
 
 npx sequelize-typescript-generator@^4 --host ${MYSQL_HOST} --database ${MYSQL_DB} --username ${MYSQL_USER} --password ${MYSQL_PASS} --port ${MYSQL_PORT} --dialect mysql --skip-tables DBMeta,AuditLog,regulationarticle --clean --out-dir ./src/models
+
+echo "WARNING: You MUST now manually review and edit the changes that occurred in 'src/models/*' BEFORE committing them"
