901a9f227aa867d12791cd673c54b96af0b1c4fd
mar <mar.nospam@anomy.net>
2021-10-01 13:04:16 +0000

    feat: Export

--- src/routes/types.ts
+++ src/routes/types.ts
@@ -1,51 +1,14 @@
-declare const _RegName__Brand: unique symbol;
-/** Regulation name — `0123/2012` */
-export type RegName = string & { [_RegName__Brand]: true };
-
-declare const _RegNameQuery__Brand: unique symbol;
-/** Regulation name formatted for URL param insertion — `0123-2012` */
-export type RegQueryName = string & { [_RegNameQuery__Brand]: true };
-
-declare const _ISODate__Brand: unique symbol;
-/** Valid ISODate string — e.g. `2012-09-30` */
-export type ISODate = string & { [_ISODate__Brand]: true };
-
-declare const _ISODateTime__Brand: unique symbol;
-/** Valid UTC ISODateTime string — e.g. `2012-09-30T12:00:00` */
-export type ISODateTime = string & { [_ISODateTime__Brand]: true };
-
-declare const _HTMLText__Brand: unique symbol;
-/** HTMLText string — e.g. `I &lt;3 You ` */
-export type HTMLText = string & { [_HTMLText__Brand]: true };
-/** Plain-text string — e.g. `I <3 You ` */
-export type PlainText = string & { [_HTMLText__Brand]?: false };
-
-declare const _MinistrySlug__Brand: unique symbol;
-/** Slug identifier for Ministries — e.g. `hr`
- *
- * (Human-readable, persistent id for for query strings, etc.)
- */
-export type MinistrySlug = string & { [_MinistrySlug__Brand]: true };
-
-declare const _LawChapterSlug__Brand: unique symbol;
-/** Slug identifier for Law Chapters — e.g. `01b`
- *
- * (Human-readable, persistent id for for query strings, etc.)
- */
-export type LawChapterSlug = string & { [_LawChapterSlug__Brand]: true };
-
-/** Regulations are roughly classified based on whether they contain
- * any original text/stipulations, or whether they **only**  prescribe
- * changes to other regulations.
- *
- * `base` = Stofnreglugerð
- * `amending` = Breytingareglugerð
- */
-export type RegulationType = 'base' | 'amending';
-
-declare const _Year__Brand: unique symbol;
-/** Four letter positive integer that might reasonably be a year */
-export type Year = number & { [_Year__Brand]: true };
+import {
+  LawChapterSlug,
+  MinistrySlug,
+  RegName,
+  ISODate,
+  PlainText,
+  HTMLText,
+  RegulationType,
+  Year,
+} from '@hugsmidjan/regulations-editor/types';
+export * from '@hugsmidjan/regulations-editor/types';
 
 // ---------------------------------------------------------------------------
 
