18f7131897222fff56741a0c244b867e3df5b0bd
mar <mar.nospam@anomy.net>
2021-10-13 10:37:39 +0000

    chore: Remove now redundant eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,6 @@
 module.exports = require('@hugsmidjan/hxmstyle')({
   // Place your project-specific additions or overrides here
   // using standard ESLint config syntax...
-  rules: {
-    'destructuring/no-rename': 0,
-  },
 
   parserOptions: {
     ecmaVersion: 2020,
