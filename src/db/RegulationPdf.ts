8aeb9ce8b7b341fed8867240e01f8d195fd3ac0f
mar <mar.nospam@anomy.net>
2021-11-09 13:14:59 +0000

    fix: Valid date-range for "original" versions of regulations

--- src/db/RegulationPdf.ts
+++ src/db/RegulationPdf.ts
@@ -66,7 +66,8 @@ const getStatusText = (regulation: RegulationMaybeDiff): string => {
   const {
     timelineDate,
     lastAmendDate,
-    publishedDate,
+    effectiveDate,
+    // publishedDate,
     history,
     showingDiff,
     repealed,
@@ -111,7 +112,7 @@ const getStatusText = (regulation: RegulationMaybeDiff): string => {
   }
 
   if (!timelineDate || timelineDate === lastAmendDate) {
-    const fmtLastModified = fmt(lastAmendDate || publishedDate);
+    const fmtLastModified = fmt(lastAmendDate || effectiveDate);
 
     if (repealed) {
       return (
@@ -123,8 +124,10 @@ const getStatusText = (regulation: RegulationMaybeDiff): string => {
   }
 
   const nextTimelineDate = (() => {
-    const idx = history.findIndex((item) => item.date === timelineDate);
-    const nextItem = idx > -1 && history[idx + 1];
+    const idx = [{ date: effectiveDate }]
+      .concat(history)
+      .findIndex((item) => item.date === timelineDate);
+    const nextItem = idx > -1 && history[idx];
     return nextItem ? nextItem.date : undefined;
   })();
   const fmtDateFrom = fmt(timelineDate);
