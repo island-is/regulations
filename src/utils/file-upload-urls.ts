e888100fc08f0f0902de1dae117db2c05ef574ba
Valur Sverrisson <valur@hugsmidjan.is>
2021-12-07 15:01:16 +0000

    feat: split up fileupload route

---
+++ src/utils/file-upload-urls.ts
@@ -0,0 +1,133 @@
+import S3 from 'aws-sdk/clients/s3';
+import file_type from 'file-type';
+import fetch from 'node-fetch';
+import { PassThrough, Readable } from 'stream';
+import { FastifyRequest } from 'fastify';
+
+import {
+  FILE_SERVER,
+  AWS_BUCKET_NAME,
+  AWS_REGION_NAME,
+  MEDIA_BUCKET_FOLDER,
+  OLD_SERVER,
+} from '../constants';
+import {
+  assertUploadType,
+  DRAFTS_FOLDER,
+  QUERY_REPLACEMENT,
+} from 'routes/fileUploadRoutes';
+
+// Stupid cloning for stupid streams
+const stupidStreamClone = (stream: Readable) =>
+  new Promise<[Readable, Readable]>((resolve, reject) => {
+    const b1: Array<Buffer> = [];
+    const b2: Array<Buffer> = [];
+    stream
+      .on('data', (data: Buffer) => {
+        b1.push(data);
+        b2.push(data);
+      })
+      .on('end', () => {
+        const s1 = Readable.from(b1).pipe(new PassThrough());
+        const s2 = Readable.from(b2).pipe(new PassThrough());
+        resolve([s1, s2]);
+      })
+      .on('error', (error) => {
+        reject(error);
+      });
+  });
+
+const makeFileKey = (rawUrl: string, req: FastifyRequest) => {
+  try {
+    let fullUrl = ('' + rawUrl).replace(`${DRAFTS_FOLDER}/`, '');
+    if (/^\//.test(fullUrl)) {
+      fullUrl = OLD_SERVER + fullUrl;
+    }
+
+    const { hostname, pathname } = new URL(
+      fullUrl.replace(/\?/g, QUERY_REPLACEMENT),
+    );
+
+    const pathPrefix =
+      hostname === 'www.stjornartidindi.is'
+        ? 'stjornartidindi/'
+        : hostname !== FILE_SERVER && hostname !== OLD_SERVER
+        ? `ext/${hostname}/`
+        : '';
+
+    const devFolder = MEDIA_BUCKET_FOLDER || '';
+    const rootFolder =
+      assertUploadType(req) === 'draft' && !fullUrl.startsWith(FILE_SERVER)
+        ? DRAFTS_FOLDER
+        : '';
+
+    const fileKey = `/${devFolder}/${rootFolder}/${pathPrefix}/${pathname}`
+      // remove double slashes
+      .replace(/\/\/+/g, '/');
+
+    return FILE_SERVER + fileKey;
+  } catch (error) {
+    console.error({ error });
+    return '';
+  }
+};
+
+function ensureObject(cand: unknown): Record<string, unknown> | undefined {
+  if (cand && typeof cand === 'object' && !Array.isArray(cand)) {
+    return cand as Record<string, unknown>;
+  }
+}
+function ensureStringArray(cand: unknown): Array<string> | undefined {
+  if (Array.isArray(cand) && !cand.find((item) => typeof item !== 'string')) {
+    return cand.find((u) => !!u) as Array<string>;
+  }
+}
+
+type FileUrlMapping = { oldUrl: string; newUrl: string };
+export const fileUrlsMapper = (req: FastifyRequest) => {
+  const fileUrls: Array<FileUrlMapping> = [];
+  const bdy = ensureObject(req.body) || {};
+  const links = ensureStringArray(bdy.urls) || [];
+
+  links.forEach((url) => {
+    fileUrls.push({ oldUrl: url, newUrl: makeFileKey(url, req) });
+  });
+
+  return fileUrls;
+};
+
+export const uploadFile = async (file: FileUrlMapping) => {
+  const doLog = !!MEDIA_BUCKET_FOLDER || process.env.NODE_ENV !== 'production';
+  const fileKey = file.newUrl.replace(FILE_SERVER, '');
+
+  try {
+    const s3 = new S3({ region: AWS_REGION_NAME });
+    const res = await fetch(file.oldUrl);
+    if (!res.ok) {
+      throw new Error(`Error fetching '${file.oldUrl}' (${res.status})`);
+    }
+    const [fileA, fileB] = await stupidStreamClone(res.body as Readable);
+
+    const fileType = await file_type.fromStream(fileA);
+
+    await s3
+      .upload({
+        Bucket: AWS_BUCKET_NAME,
+        Key: fileKey,
+        ACL: 'public-read',
+        ContentType: fileType?.mime,
+        Body: fileB,
+      })
+      .promise()
+      .then((data) => {
+        doLog &&
+          console.info('🆗 Uploaded', {
+            oldUrl: file.oldUrl,
+            key: data.Key,
+          });
+      });
+  } catch (error) {
+    const message = error instanceof Error ? error.message : error;
+    console.error('⚠️ ', message);
+  }
+};
---
+++ src/utils/file-upload.ts
@@ -0,0 +1,173 @@
+import S3 from 'aws-sdk/clients/s3';
+import multer from 'fastify-multer';
+import sharp from 'sharp';
+import multerS3 from 'multer-s3-transform';
+import { createHash } from 'crypto';
+import { Readable } from 'stream';
+import type { Request as ExpressRequest } from 'express';
+import { assertUploadType, DRAFTS_FOLDER } from 'routes/fileUploadRoutes';
+import {
+  AWS_BUCKET_NAME,
+  AWS_REGION_NAME,
+  MEDIA_BUCKET_FOLDER,
+} from '../constants';
+
+// `multer-s3-transform` doesn't have TypeScript definitions, so we just make do with this
+// copied over from https://www.npmjs.com/package/multer-s3-transform#file-information
+type MulterFile = Omit<
+  Express.Multer.File,
+  'filename' | 'destination' | 'path' | 'buffer' | 'stream'
+>;
+
+export type MulterS3StorageFile = MulterFile & {
+  /** The bucket used to store the file */
+  bucket: string;
+  /** The name of the file */
+  key: string;
+  /** Access control for the file */
+  acl: string;
+  /** The mimetype used to upload the file */
+  contentType: string;
+  /** The metadata object to be sent to S3 */
+  metadata: string;
+  /** The S3 url to access the file */
+  location: string;
+  /** The etagof the uploaded file in S3 */
+  etag: string;
+  /** The contentDisposition used to upload the file */
+  contentDisposition: string;
+  /** The storageClass to be used for the uploaded file in S3 */
+  storageClass: string;
+  /** The versionId is an optional param returned by S3 for versioned buckets. */
+  versionId: string;
+};
+
+type MulterFileWithHash = MulterFile & {
+  $hash$?: string;
+  isPasted?: boolean;
+  key?: string;
+};
+
+const getSingleQuery = (req: ExpressRequest, param: string): string => {
+  let value = req.query[param] as string | Array<string> | undefined;
+  if (value && typeof value !== 'string') {
+    value = value[0];
+  }
+  return value || '';
+};
+
+const dotPathRe = /\.+(?:\/|$)/g;
+
+const getKey = (req: ExpressRequest, _file: MulterFile) => {
+  const file = _file as MulterFileWithHash;
+
+  const folder = getSingleQuery(req, 'folder').replace(dotPathRe, '/');
+
+  const rootFolder = assertUploadType(req) === 'draft' ? DRAFTS_FOLDER : '';
+  const devFolder = MEDIA_BUCKET_FOLDER || '';
+  const originalName = file.originalname.split('/').pop() as string;
+  let fileNamePart = originalName.replace(/\.[^.]+$/, '');
+  const fileExtension = originalName.slice(fileNamePart.length).toLowerCase();
+  if (/^pasted--image/.test(fileNamePart)) {
+    file.isPasted = true;
+    fileNamePart = fileNamePart.replace(/^pasted--/, '');
+  }
+  const hash = file.$hash$ ? '--' + file.$hash$ : '';
+
+  const fileName = `${fileNamePart}${hash}${fileExtension}`;
+
+  const fileUrl = `/${devFolder}/${rootFolder}/files/${folder}/${fileName}`
+    // remove double slashes
+    .replace(/\/\/+/g, '/')
+    .replace(/^\//, '');
+
+  return fileUrl;
+};
+
+const storage = multerS3({
+  s3: new S3({ region: AWS_REGION_NAME }),
+  bucket: AWS_BUCKET_NAME || '',
+  acl: 'public-read',
+  contentType: multerS3.AUTO_CONTENT_TYPE,
+  key: (req, file, cb) => {
+    cb(null, getKey(req as ExpressRequest, file));
+  },
+  // See also: https://www.npmjs.com/package/multer-s3-transform#transforming-files-before-upload
+  // @ts-expect-error  (multer-s3-transform has no .d.ts files)
+  shouldTransform: function (req, file: MulterFileWithHash, cb) {
+    // cb(null, false); // no transforming for the time being. Deafult to all pasted files being just bulky PNG
+    cb(null, file.isPasted);
+  },
+  transforms: [
+    {
+      id: 'original',
+      key: (
+        req: ExpressRequest,
+        file: MulterFileWithHash,
+        // @ts-expect-error  (multer-s3-transform has no .d.ts files)
+        cb,
+      ) => {
+        cb(null, getKey(req, file).replace(/\.png$/, '.jpg'));
+      },
+      // @ts-expect-error  (multer-s3-transform has no .d.ts files)
+      transform: (req: Request, file, cb) => {
+        cb(
+          null,
+          sharp().flatten({ background: '#ffffff' }).jpeg({ quality: 85 }),
+        );
+      },
+    },
+    // {
+    //   id: 'webp nearLossless',
+    //   // @ts-expect-error  (multer-s3-transform has no .d.ts files)
+    //   key: (req: Request, file: MulterFileWithHash, cb) => {
+    //     cb(null, getKey(req, file).replace(/\.png$/, '-nll.webp'));
+    //   },
+    //   // @ts-expect-error  (multer-s3-transform has no .d.ts files)
+    //   transform: (req: Request, file, cb) => {
+    //     cb(null, sharp().webp({ quality: 85, nearLossless: true }));
+    //   },
+    // },
+    // {
+    //   id: 'webp lossy',
+    //   // @ts-expect-error  (multer-s3-transform has no .d.ts files)
+    //   key: (req: Request, file: MulterFileWithHash, cb) => {
+    //     cb(null, getKey(req, file).replace(/\.png$/, '.webp'));
+    //   },
+    //   // @ts-expect-error  (multer-s3-transform has no .d.ts files)
+    //   transform: (req: Request, file, cb) => {
+    //     cb(null, sharp().webp({ quality: 85 }));
+    //   },
+    // },
+  ],
+});
+
+const multerS3_handleFile = storage._handleFile;
+storage._handleFile = function (req, file, cb) {
+  const _this = this;
+  const hash = createHash('md5');
+  const fileData: Array<Buffer> = [];
+  file.stream
+    .on('data', (data: Buffer) => {
+      hash.update(data);
+      fileData.push(data);
+    })
+    .on('end', () => {
+      const $hash$ = hash.digest('hex').substr(0, 8);
+      multerS3_handleFile.call(
+        _this,
+        req,
+        {
+          ...file,
+          $hash$,
+          stream: Readable.from(fileData),
+        },
+        cb,
+      );
+    });
+};
+
+export const fileUploader = multer({
+  // @ts-expect-error  (multer-s3-transform assumes Express.js-branded Response/Request objects)
+  storage,
+}).single('file');
