754b094db7aadffc5ad88d2f6e635484c5acf58d
mar <mar.nospam@anomy.net>
2021-12-14 01:23:55 +0000

    feat: Remove non-sensical mapper functions from `utils`

--- packages/tools/CHANGELOG.md
+++ packages/tools/CHANGELOG.md
@@ -3,6 +3,7 @@
 ## Upcoming...
 
 - ... <!-- Add new lines here. -->
+- feat: Remove non-sensical mapper functions from `utils`
 
 ## 0.5.68
 
--- packages/tools/src/_utils/urls.ts
+++ packages/tools/src/_utils/urls.ts
@@ -2,22 +2,6 @@ import { RegName, RegQueryName } from '../types';
 
 /** Converts a Regulation `name` into a URL path segment
  *
- *  Example: '0123/2020' --> '2020/0123'
- */
-export const nameToPath = (regulationName: string, seperator?: string) => {
-  const [number, year] = regulationName.split('/');
-  return year + (seperator || '/') + number;
-};
-
-/** Creates a bare task URL for a given Regulation `name`
- *
- *  Example: '0123/2020' --> '/task/2020/0123'
- */
-export const taskUrl = (regulationName: string) =>
-  '/task/' + nameToPath(regulationName);
-
-/** Converts a Regulation `name` into a URL path segment
- *
  *  Example: '0123/2020' --> '0123-2020'
  */
 export const nameToSlug = (regulationName: RegName): RegQueryName =>
